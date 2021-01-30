0aae3131e604721f7bfe89e2c3691ac57cddabc8
jrnbulu <jrn.bulu@gmail.com>
2021-01-30 21:47:26 +0530

    test in dev

--- coreJavaApp/src/com/bss/arrays/Array2D.java
+++ coreJavaApp/src/com/bss/arrays/Array2D.java
@@ -1,7 +1,7 @@
 package com.bss.arrays;
 
 public class Array2D {
-
+    //first commit to dev
 	public static void main(String[] args) {
 		//declaring and initializing 2D array  
 		int arr[][]={{1,2,3},{2,4,5},{4,4,5}};  
